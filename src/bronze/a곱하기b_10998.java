2723646e9cc4c8a3ec6339270d0c31418d067f3b
임진수 <jsbd0109@naver.com>
2025-09-23 23:02:10 +0900

    10998번

---
+++ src/bronze/a곱하기b_10998.java
@@ -0,0 +1,14 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class a곱하기b_10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a * b);
+
+        sc.close();
+    }
+}
