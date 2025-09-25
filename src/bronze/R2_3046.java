d8d1e4f54fc16c05306ffcf12c30d67e164523d7
임진수 <jsbd0109@naver.com>
2025-09-26 00:25:21 +0900

    3046번

---
+++ src/bronze/R2_3046.java
@@ -0,0 +1,14 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class R2_3046 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int R1 = sc.nextInt();
+        int S = sc.nextInt();
+
+        System.out.println((S * 2) - R1);
+    }
+}
